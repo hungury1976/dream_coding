9b8f2b1a0354f812681b5b7a5be682fb1b2d9850
hungury1976 <hungury76@runigen.com>
2021-10-25 20:09:43 +0900

    HTML/CSS,브라우저101 과정 산출물

---
+++ browser/carrot game/src/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import { GameBuilder, Reason } from './game.js';
+import PopUp from './popup.js';
+import * as sound from './sound.js';
+
+const gameFinishBanner = new PopUp();
+const game = new GameBuilder()
+    .withGameDuration(5)
+    .withCarrotCount(3)
+    .withBugCount(3)
+    .build();
+
+game.setGameStopListener(reason => {
+    console.log(reason);
+    let message;
+    switch(reason) {
+        case Reason.cancel:
+            message = 'Replay?';
+            sound.playAlert();
+            break;
+        case Reason.win:
+            message = 'You Won!';
+            sound.playWin();
+            break;
+        case Reason.lose:
+            message = 'You Lost';
+            sound.playBug();
+            break;
+        default:
+            throw new Error('not valid reason');
+    }
+    gameFinishBanner.showWithText(message);
+});
+
+gameFinishBanner.setClickListener(() => {
+    game.start();
+});
